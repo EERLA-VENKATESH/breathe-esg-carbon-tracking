82e5ad9d3627264e6df49c212a0ceb0f8da40553
Eerla Venkatesh <venkatesheerla799@gmail.com>
2026-05-26 15:04:39 +0530

    Upgrade to professional welcome page with live stats

--- breathe_esg/urls.py
+++ breathe_esg/urls.py
@@ -1,25 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from django.views.generic import RedirectView
-
-# Simple welcome page for root URL
-def welcome(request):
-    return HttpResponse("""
-        <h1>Breathe ESG Carbon Tracking System</h1>
-        <p>Welcome to the Carbon Emissions Tracking API!</p>
-        <ul>
-            <li><a href='/api/'>API Root</a> - View all API endpoints</li>
-            <li><a href='/api/normalized-records/'>Normalized Records</a> - View calculated emissions</li>
-            <li><a href='/api/dashboard/1/'>Dashboard</a> - Review workflow statistics</li>
-            <li><a href='/admin/'>Admin Panel</a> - Login with admin/admin123</li>
-        </ul>
-        <hr>
-        <p><strong>Login Credentials:</strong> admin / admin123</p>
-    """)
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ingestion.urls')),
-    path('', welcome),  # This fixes the root URL 404
+    path('', TemplateView.as_view(template_name='ingestion/welcome.html'), name='welcome'),
 ]
